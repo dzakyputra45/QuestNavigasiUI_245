b48fcae8d662ffa46974f097aede5553ff07d2de
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-06 09:15:22 +0700

    Tambahkan DataApp composable dengan NavHostController

--- app/src/main/java/com/example/appnavigation/Navigasiku.kt
+++ app/src/main/java/com/example/appnavigation/Navigasiku.kt
@@ -1,6 +1,20 @@
 package com.example.appnavigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulir,
     Ddetail
+}
+
+
+@Composable
+fun DataApp(
+    navController : NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+
 }
